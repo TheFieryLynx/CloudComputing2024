43720c88e600e9bb267e865224803b317671f0c8
Andrew Biryukov <biryukov.andrej2000@ispras.ru>
2024-03-16 20:40:11 +0300

    add get_users route

--- api/src/api/router.py
+++ api/src/api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 
-from src.database.database import create_user, get_user_by_id
+from src.database.database import create_user, get_user_by_id, get_users
 from src.models.user import User
 
 router = APIRouter()
@@ -19,9 +19,20 @@ async def add_user(email: str, first_name: str, last_name: str):
 @router.get("/users/{user_id}")
 async def add_user(user_id: int):
     data = get_user_by_id(user_id)
-    print(data)
 
     if len(data) > 0:
         return User(user_dict=data[0])
     else:
-        raise HTTPException(status_code=500, detail="User not found")
+        raise HTTPException(status_code=404, detail="User not found")
+
+
+@router.get("/users/")
+async def add_user():
+    data = get_users()
+    result = []
+    for user in data:
+        result.append(User(user_dict=user))
+    if len(data) > 0:
+        return result
+    else:
+        raise HTTPException(status_code=404, detail="Users not found")
--- api/src/database/database.py
+++ api/src/database/database.py
@@ -34,9 +34,34 @@ def get_user_by_id(user_id: int):
         raise
     cursor = conn.cursor()
     query = "SELECT * FROM users WHERE id = %s"
-    values = (user_id, )
+    values = (user_id,)
     try:
         cursor.execute(query, values)
+    except Exception as e:
+        print(f"Failed to execute query:", e)
+    result = []
+    columns = [column[0] for column in cursor.description]
+    for row in cursor.fetchall():
+        result.append(dict(zip(columns, row)))
+    try:
+        conn.close()
+        cursor.close()
+    except Exception as e:
+        print(f"Failed to close mysql session:", e)
+        raise
+    return result
+
+
+def get_users():
+    try:
+        conn = connect()
+    except Exception as e:
+        print(f"Failed to connect to mysql database:", e)
+        raise
+    cursor = conn.cursor()
+    query = "SELECT * FROM users"
+    try:
+        cursor.execute(query)
     except Exception as e:
         print(f"Failed to execute query:", e)
     result = []
